2eb827317769d04e777b21c38b07d5a751937f10
Amberjack10 <sanghwasong10@gmail.com>
2024-02-28 14:31:10 +0900

    Featur : Enemy Move 구현

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public enum EnemyState
 {
@@ -19,32 +22,34 @@ public class EnemyController : MonoBehaviour, IDamageable
     [field: Header("Animations")]
     [field: SerializeField] public AnimationData AnimationData { get; private set; }
 
+    public Transform Target { get; private set; }
     public Rigidbody Rigidbody { get; private set; }
     public Animator Animator { get; private set; }
-    public CharacterController Controller { get; private set; }
+    public EnemyMove EnemyMove { get; private set; }
 
-    private EnemyStateMachine stateMachine;
     public Transform movePoint;
     public float moveSpeed = 5f;
 
     public bool isMonsterTurn = false;
     public bool isTurnOver = false;
 
+    public EnemyState currentState;
+
+    private int localTurn;
+
     private void Awake()
     {
         AnimationData.Initialize();
 
+        Target = GameObject.FindGameObjectWithTag("Player").transform;
         Rigidbody = GetComponent<Rigidbody>();
         Animator = GetComponentInChildren<Animator>();
-        Controller = GetComponent<CharacterController>();
-
-        stateMachine = new EnemyStateMachine(this);
+        EnemyMove = GetComponent<EnemyMove>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        stateMachine.ChangeState(stateMachine.E_IdleState);
         movePoint.parent = null;
 
         TuenManager.I.MonsterTurn += UpdateMonsterTurn;
@@ -55,27 +60,88 @@ public class EnemyController : MonoBehaviour, IDamageable
     {
         if (isMonsterTurn)
         {
-            stateMachine.Update();
+            UpdateState();
         }
         else
             EndOfMonsterTurn();
     }
 
+    public void SetEnemyState(EnemyState state)
+    {
+        switch (state)
+        {
+            case EnemyState.Idle:
+                currentState = EnemyState.Idle;
+                break;
+            case EnemyState.Chasing:
+                currentState = EnemyState.Chasing;
+                break;
+        }
+    }
+
+    public void UpdateState()
+    {
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                CheckPlayerInRange();
+                break;
+            case EnemyState.Chasing:
+                EnemyMove.Move();
+                break;
+            case EnemyState.Attacking:
+                break;
+        }
+    }
+
+    private void CheckPlayerInRange()
+    {
+        if (IsInAttackRange())
+        {
+            if (!IsTurnOver(EnemyState.Attacking))
+                SetEnemyState(EnemyState.Attacking);
+            else
+                return;
+        }
+        else if (IsInChasingRange())
+        {
+            if (!IsTurnOver(EnemyState.Chasing))
+                SetEnemyState(EnemyState.Chasing);
+            else
+                return;
+        }
+    }
+
+    private bool IsInChasingRange()
+    {
+        float playerDistanceSqr = (Target.transform.position - transform.position).sqrMagnitude;
+        return playerDistanceSqr <= EnemyData.enemyDetectRange * EnemyData.enemyDetectRange;
+    }
+
+    private bool IsInAttackRange()
+    {
+        float playerDistanceSqr = (Target.transform.position - transform.position).sqrMagnitude;
+        return playerDistanceSqr <= EnemyData.enemyAttackRange * EnemyData.enemyAttackRange;
+    }
+
     public void TakeDamage(int damage)
     {
-
     }
 
     private void UpdateMonsterTurn(int turn)
     {
         movePoint.parent = null;
         movePoint.position = SetEnemyMovePoint();
+
+        localTurn = turn;
         isMonsterTurn = true;
+
+        SetEnemyState(EnemyState.Idle);
     }
 
     private Vector3 SetEnemyMovePoint()
     {
-        Vector3 movePoint = (stateMachine.Target.transform.position - stateMachine.EnemyController.transform.position).normalized;
+        Vector3 movePoint = (Target.transform.position - transform.position).normalized;
 
         if (Mathf.Abs(movePoint.x) < 0.5f) movePoint.x = 0f;
         else movePoint.x = movePoint.x > 0 ? 1f : -1f;
@@ -89,10 +155,30 @@ public class EnemyController : MonoBehaviour, IDamageable
         return movePoint;
     }
 
+    public bool IsTurnOver(EnemyState state)
+    {
+        switch(state)
+        {
+            case EnemyState.Chasing:
+                if(localTurn < EnemyData.enemyMoveCost)
+                {
+                    return true;
+                }
+                return false;
+            case EnemyState.Attacking:
+                if(localTurn < EnemyData.enemyAttackCost)
+                {
+                    return true;
+                }
+                return false;
+                default: return false;
+        }
+    }
+
     private void EndOfMonsterTurn()
     {
         movePoint.parent = gameObject.transform;
-        stateMachine.EnemyIsMoved(false);
+
         isTurnOver = false;
     }
 
---
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private EnemyController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<EnemyController>();
+    }
+
+    public void Move()
+    {
+        Move(_controller.movePoint.position);
+    }
+
+    private void Move(Vector3 moveDirection)
+    {
+        _controller.movePoint.position = moveDirection;
+        _controller.transform.position = Vector3.MoveTowards(_controller.transform.position, _controller.movePoint.position, _controller.moveSpeed * Time.deltaTime);
+
+        if (_controller.transform.position.Equals(_controller.movePoint.position))
+        {
+            _controller.IsTurnOver(EnemyState.Chasing);
+            _controller.SetEnemyState(EnemyState.Idle);
+        }
+    }
+}
