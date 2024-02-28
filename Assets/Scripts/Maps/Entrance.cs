27fd5c70ade968aaa580b668861fc59002189e4b
PHT97 <park9704271@naver.com>
2024-02-28 16:55:01 +0900

    <Fix> 맵제작 중간작업

--- Assets/Scripts/Maps/Entrance.cs
+++ Assets/Scripts/Maps/Entrance.cs
@@ -6,20 +6,16 @@ public class Entrance : MonoBehaviour
 {
     [SerializeField] MakeRandomMap makeRandomMap;
     [SerializeField] private GameObject MoveUI;
-
-    //클리어한 방의 개수
-    private int clearRoomNum;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //TODO
         //플레이어 받으면 TAG로 변경
         //플레이어가 계단에 닿으면 팝업? 선택지? 출현
-        if(collision.gameObject.name == "Player") 
+        if (collision.gameObject.name == "Player")
         {
             MoveUI.SetActive(true);
             StartCoroutine(PuzGame());
         }
-        Debug.Log(clearRoomNum);
     }
 
     IEnumerator PuzGame()
@@ -30,8 +26,8 @@ public class Entrance : MonoBehaviour
 
     public void YesButton()
     {
+        makeRandomMap.PlusCount();
         makeRandomMap.StartRandomMap();
-        clearRoomNum++;
         MoveUI.SetActive(false);
         Time.timeScale = 1;
     }
