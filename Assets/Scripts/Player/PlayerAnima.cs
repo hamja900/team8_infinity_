ac68c2f55eefa7e3bc6dc0766ad84c4a740e5972
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-03-02 03:07:51 +0900

    Fix - Player연속 공격

--- Assets/Scripts/Player/PlayerAnima.cs
+++ Assets/Scripts/Player/PlayerAnima.cs
@@ -24,28 +24,39 @@ public class PlayerAnima : MonoBehaviour
     }
     public void AttackAnima(bool b)
     {
+        StartCoroutine(WaitAnima(b));
+    }
+    IEnumerator WaitAnima(bool b)
+    {
         IsAttackAnima = true;
         SpriteFileX(b);
         ani.SetTrigger("IsAttack");
-    }
-    private void Update()
-    {
-        if (IsAttackAnima)
+        b = true;
+        while (b)
         {
             if (ani.GetCurrentAnimatorStateInfo(0).IsName("Player_Attack") == true)
             {
                 float animTime = ani.GetCurrentAnimatorStateInfo(0).normalizedTime;
                 if (animTime >= 1f)
                 {
-                    Pattack.AttackEvent();
-                    IsAttackAnima = false;
-                    return;
+                    b = false;
                 }
             }
+            yield return null;
+        }
+        while (!b)
+        {
+            if (ani.GetCurrentAnimatorStateInfo(0).IsName("Player_Idle") == true)
+            {
+                Pattack.AttackEvent();
+                IsAttackAnima = false;
+                break;
+            }
+            yield return null;
         }
     }
     public void MoveAnima(bool b)
     {
-        ani.SetBool("IsMove",b);
+        ani.SetBool("IsMove", b);
     }
 }
